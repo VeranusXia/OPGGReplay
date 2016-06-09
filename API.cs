92599371d88e4b74d6d93283de2d479e44c8dc30
Veranus Xia <veranus@vip.qq.com>
2016-06-09 15:40:03 +0800

    demo for opggReplay searcher
    demo for opggReplay searcher

---
+++ API.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace OPGGReplay
+{
+    public class API
+    {
+        public string apiKey { get; set; }//	
+        public Matches GetMatchListBySummonerName(string name)
+        {
+            string id = GetIdByName(name);
+            return GetMatchListBySummonerId(id);
+        }
+        public string GetIdByName(string name)
+        {
+            try
+            {
+                string url = string.Format("https://kr.api.pvp.net/api/lol/kr/v1.4/summoner/by-name/{0}?api_key={1}", name, apiKey);
+                using (WebClient wc = new WebClient())
+                {
+                    byte[] result = wc.DownloadData(url);
+                    string r = Encoding.UTF8.GetString(result);
+                    JObject obj = JsonConvert.DeserializeObject<JObject>(r);
+                    return obj[name.Replace(" ", "").Trim().ToLower()]["id"].ToString(); 
+
+                }
+            }
+            catch
+            {
+                return "";
+            }
+        }
+        public Matches GetMatchListBySummonerId(string id)
+        {
+            try
+            {
+                string url = string.Format("https://kr.api.pvp.net/api/lol/kr/v2.2/matchlist/by-summoner/{0}?api_key={1}", id, apiKey);
+                using (WebClient wc = new WebClient())
+                {
+                    byte[] result = wc.DownloadData(url);
+                    string r = Encoding.UTF8.GetString(result);
+                    Matches obj = JsonConvert.DeserializeObject<Matches>(r);
+                    return obj;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public void DownloadBat(string matchId)
+        {
+            if (!Directory.Exists(System.Environment.CurrentDirectory + "\\Replay\\"))
+            {
+                Directory.CreateDirectory(System.Environment.CurrentDirectory + "\\Replay\\");
+            }
+            using (WebClient wc = new WebClient())
+            {
+                string url = string.Format("http://www.op.gg/match/new/id={0}", matchId);
+                wc.DownloadFile(url, System.Environment.CurrentDirectory + "\\Replay\\" + matchId + ".bat");
+            }
+        }
+    }
+    public class Matches
+    {
+        public Match[] matches { get; set; }
+    }
+    public class Match
+    {
+        public string region { get; set; }
+        public string platformId { get; set; }
+        public long matchId { get; set; }
+        public int champion { get; set; }
+        public string queue { get; set; }
+        public string season { get; set; }
+        public long timestamp { get; set; }
+        public string lane { get; set; }
+        public string role { get; set; }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OPGGReplay
+{
+    public partial class Form1 : Form
+    {
+        private API api = new API();
+        private Matches matches = new Matches();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            api.apiKey = textBox2.Text;
+            matches = api.GetMatchListBySummonerName(name);
+            dataGridView1.DataSource = matches.matches.Select(u => new
+            {
+                服务器 = u.platformId,
+                英 = GetImageFromWeb(u.champion.ToString()),
+                比赛 = u.matchId,
+                分路 = u.lane,
+                位置 = u.role
+            }).ToList();
+            DataGridViewImageColumn column = (DataGridViewImageColumn)dataGridView1.Columns[1];
+            column.ImageLayout = DataGridViewImageCellLayout.Stretch;
+            column.Width = 25;
+
+        }
+        public Image GetImageFromWeb(string cid)
+        {
+            string url = string.Format("http://img.db.178.com/lol/images/content/champion/icons/{0}.jpg", cid);
+            string filepath = System.Environment.CurrentDirectory + "\\img\\" + cid + ".jpg";
+            if (File.Exists(filepath))
+            {
+                return GetImage(filepath);
+            }
+            else
+            {
+                if (!Directory.Exists(System.Environment.CurrentDirectory + "\\img\\"))
+                {
+                    Directory.CreateDirectory(System.Environment.CurrentDirectory + "\\img\\");
+                }
+                using (WebClient wc = new WebClient())
+                {
+                    wc.DownloadFile(url, filepath);
+                    return GetImage(filepath);
+                }
+            }
+
+        }
+        public System.Drawing.Image GetImage(string path)
+        {
+            System.IO.FileStream fs = new System.IO.FileStream(path, System.IO.FileMode.Open);
+            System.Drawing.Image result = System.Drawing.Image.FromStream(fs);
+
+            fs.Close();
+
+            return result;
+
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string matchid = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            api.DownloadBat(matchid);
+
+            Process.Start(System.Environment.CurrentDirectory + "\\Replay\\" + matchid + ".bat");
+        }
+
+    }
+}
